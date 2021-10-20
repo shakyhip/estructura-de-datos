2f2427791a2fa50c184be6818620b933441270b5
shakyhip <shakyhip@users.noreply.github.com>
2021-10-19 20:09:02 -0500

    ejericios de pilas

--- stack/ejercicios/ejercicios.js
+++ stack/ejercicios/ejercicios.js
@@ -6,22 +6,46 @@ Entrada : mifuncion([‘Manzana’,‘Cebolla’,‘Apio’,‘Naranja’,‘Pap
 Salida: [‘Manzana’,‘Cebolla’,‘Apio’,‘Naranja’]
 */
 
+function ajusteDePila(pila, numero) {
+    for (let i = 0; pila.length != numero; i++) {
+        pila.pop();
+    }
+    return pila;
+}
+
+var nuevaPila = ajusteDePila(['Manzana', 'Cebolla', 'Apio', 'Naranja', 'Papaya', 'Sandía', 'Melón', 'Pepino', 'Calabaza', 'Uvas'], 4);
+console.log(nuevaPila);
+
 
 /*
 2.- Escribe una función “reemplazar” que tenga como parámetros una pila de elementos de tipo Number,
-y dos valores también de tipo Number “nuevo” y “viejo”, de forma que si el segundo valor aparece en algún 
+y dos valores también de tipo Number “nuevo” y “viejo”, de forma que si el segundo valor aparece en algún
 lugar de la pila, sea reemplazado por el primero (Solo la primera vez), y hará pop de los elementos más nuevos.
 
 Entrada: reemplazar([3,2,3,4,6,8,1,2,5,5], 7, 2)
 Salida: [3,2,3,4,6,8,1,7]
-*/ 
+*/
+
+function reemplazar(pila, nuevo, viejo) {
+
+    //proceso
+    while (pila[pila.length - 1] != viejo) {
+        pila.pop();
+    }
+    pila.pop();
+    pila.push(nuevo)
+    return pila;
+}
+
+var remp = reemplazar([3,2,3,4,6,8,1,2,5,5], 7, 2);
+console.log(remp);
 
 
 
 /*
 3.- Un conductor maneja de un pueblo origen a un pueblo destino, pasando por varios
 pueblos. Una vez en el pueblo destino, el conductor debe regresar a casa por el mismo
-camino. Muestre el camino recorrido tanto de ida como de vuelta. 
+camino. Muestre el camino recorrido tanto de ida como de vuelta.
 
 Recorrido: Pueblo Origen → pueblo 1 → pueblo 2 → destino
 Regreso: destino → pueblo 2’ → pueblo 1 → Pueblo Origen
@@ -30,8 +54,8 @@ Regreso: destino → pueblo 2’ → pueblo 1 → Pueblo Origen
 
 
 /*
-4.- Un almacén tiene capacidad para apilar “n” contenedores. Cada contenedor tiene un número de identificación. 
-Cuando se desea retirar un contenedor específico, deben retirarse primero los contenedores que están encima de él 
+4.- Un almacén tiene capacidad para apilar “n” contenedores. Cada contenedor tiene un número de identificación.
+Cuando se desea retirar un contenedor específico, deben retirarse primero los contenedores que están encima de él
 y colocarlos en otra pila, efectuar el retiro y regresarlos a su respectivo lugar.
 Implementar en codigo esta funcionalidad.
 */
--- stack/stack.js
+++ stack/stack.js
@@ -3,142 +3,53 @@ class Stack {
     this.stack = [];
   }
   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   push(element) {
     this.stack.push(element);
     return this.stack;
   }
   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   pop() {
     return this.stack.pop();
   }
   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   peek() {
     return this.stack[this.stack.length - 1];
   }
   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   size() {
     return this.stack.length;
   }
-
-
-
-
-
-
-
-
-
-
-
 
   print() {
     console.log(this.stack);
   }
 
-
-
-
-
-
-
-
   isEmpty() {
     return this.stack.length === 0;
   }
-
-
-
-
-
 
   empty() {
     this.stack.length = 0;
   }
 
 }
-const stack = new Stack();
+
+const pila = new Stack();
+
+console.log(pila.size());
+pila.push("Azul");
+console.log(pila.size());
+pila.print();
+pila.push("Rojo");
+console.log(pila.size());
+pila.print();
+console.log(pila.peek());
+console.log(pila.isEmpty());
+pila.pop();
+console.log(pila.peek());
+pila.pop();
+console.log(pila.isEmpty());
+pila.push('Naranja');
+pila.push('Amarillo');
+pila.empty();
+pila.print();
